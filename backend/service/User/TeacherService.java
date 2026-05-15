a0f2bbecb34abf65b2a241f46519c894efeea842
Minqi-Liu <3953127855@qq.com>
2026-05-15 22:24:26 +0800

    用户业务接口和实现

---
+++ backend/service/User/TeacherService.java
@@ -0,0 +1,22 @@
+package com.shanzhu.sc.service.User;
+
+import com.shanzhu.sc.dto.User;
+import com.shanzhu.sc.utils.PagingResult;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 教师用户 业务服务层
+ */
+public interface TeacherService {
+
+    void addTeacher(User user);
+
+    void delete(List<Integer> ids);
+
+    void update(User user);
+
+    PagingResult<User> getTeacherList(RowBounds rowBounds, Map<String, Object> condition);
+}
---
+++ backend/service/User/UserService.java
@@ -0,0 +1,22 @@
+package com.shanzhu.sc.service.User;
+
+import com.shanzhu.sc.dto.User;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 用户相关 业务服务层
+ */
+public interface UserService {
+
+    User getStudentInfo(Map<String, Object> condition);
+
+    boolean update(Map<String, Object> condition);
+
+    List<Object> getTree();
+
+    String getToken(User user, long time);
+
+    User findUser(Map<String, Object> condition);
+}
