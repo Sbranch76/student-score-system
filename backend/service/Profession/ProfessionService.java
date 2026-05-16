b35a6b3baaf0211dd6716675f2353fa2cbe36073
Sbranch76 <2842496784@qq.com>
2026-05-16 16:44:07 +0800

    chore: 添加专业业务服务接口

---
+++ backend/service/Profession/ProfessionService.java
@@ -0,0 +1,20 @@
+package com.shanzhu.sc.service.Profession;
+
+import com.shanzhu.sc.domain.Profession;
+
+import java.util.List;
+
+
+/**
+ * 专业信息 业务服务层
+ *
+/
+public interface ProfessionService {
+
+    /**
+     * 获取专业
+     *
+     * @return 专业列表
+     */
+    List<Profession> getProfessionList();
+}
