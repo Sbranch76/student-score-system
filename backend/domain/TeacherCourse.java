be4fc7ee321d37f6705cf49328f411790d49bb32
Sbranch76 <2842496784@qq.com>
2026-05-17 16:53:43 +0800

    feat: 添加教师课程关联实体类 TeacherCourse

---
+++ backend/domain/TeacherCourse.java
@@ -0,0 +1,53 @@
+package com.shanzhu.sc.domain;
+
+import lombok.Data;
+
+/**
+ * 教师课程任命表实体类
+ /
+@Data
+public class TeacherCourse {
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 教师id
+     */
+    private String teacherId;
+
+    /**
+     * 教师用户名
+     */
+    private String username;
+
+    /**
+     * 课程名
+     */
+    private String name;
+
+    /**
+     * 专业
+     */
+    private String profession;
+
+    /**
+     * 班级
+     */
+    private String grade;
+
+    /**
+     * 学期
+     */
+    private Integer term;
+
+    /**
+     * 课程id
+     */
+    private String courseId;
+
+    private CourseInfo courseInfo;
+
+}
